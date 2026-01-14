345231d2885d32104cc91bfca6f0d531d6f5ceff
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2026-01-13 16:20:32 -0800

    Renamed Login Page to Profile Page

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,11 +1,11 @@
-const Login = () => {
+const Profile = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <h1 className="text-4xl font-bold text-center text-gray-900">This is the login page</h1>
+        <h1 className="text-4xl font-bold text-center text-gray-900">This is the Profile page</h1>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Profile;
