dc98572cb9ccb1d0e031697cf29d26695f2b8580
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2026-01-13 16:16:03 -0800

    added Auth0 wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import Navbar from "./Components/Navbar.jsx";
 import Homepage from "./Pages/HomePage.jsx";
 import Shop from "./Pages/Shop.jsx";
-import Login from "./Pages/Login.jsx";
+import Profile from "./Pages/Profile.jsx";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </Auth0Provider>
   );
 };
 
