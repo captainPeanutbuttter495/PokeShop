36077e49b8dcd141a80fbcd76c2558293d0c16e8
captainPeanutbuttter495 <matthew.garcia.081@my.csun.edu>
2026-01-15 23:02:01 -0800

    Added Auth0 audience, new /admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-import Navbar from "./Components/Navbar.jsx";
-import Homepage from "./Pages/HomePage.jsx";
-import Shop from "./Pages/Shop.jsx";
-import Profile from "./Pages/Profile.jsx";
+import Navbar from "./Components/Navbar";
+import HomePage from "./Pages/HomePage";
+import Shop from "./Pages/Shop";
+import Profile from "./Pages/Profile";
+import AdminDashboard from "./Pages/AdminDashboard";
 
 const App = () => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
   return (
     <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
+        audience: audience,
       }}
     >
-      <Router>
+      <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </Auth0Provider>
   );
 };
